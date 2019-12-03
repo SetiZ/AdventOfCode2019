950577cb0de31c1ebad028b8d63928d240ea6009
SetiZ <SetiZ@users.noreply.github.com>
2019-12-03 17:48:43 +0100

    missed something

--- day3/day3-1.js
+++ day3/day3-1.js
@@ -47,55 +47,6 @@ const matrixMaker = (element, bigMatrix) => {
 matrixMaker(array[0], bigMatrix1);
 matrixMaker(array[1], bigMatrix2);
 
-// array.forEach(element => {
-//   const wire = element.split(",");
-//   let lat = 0;
-//   let long = 0;
-//   wire.forEach(point => {
-//     var letr=  point.match(/[a-zA-Z]+/g).toString();
-//     var num = point.match(/\d+/g).toString();
-//     // console.log(point);
-//     switch (letr) {
-//       case 'L':
-//         for(i=0;i<num;i++) {
-//           bigMatrix.push([lat - i, long]);
-//         }
-//         lat = lat - i;
-//         break;
-//       case 'U':
-//         for(i=0;i<num;i++) {
-//           bigMatrix.push([lat, long - i]);
-//         }
-//         long = long - i;
-//         break;
-//       case 'R':
-//         for(i=0;i<num;i++) {
-//           bigMatrix.push([lat + i, long]);
-//         }
-//         lat = lat + i;
-//         break;
-//       case 'D':
-//         for(i=0;i<num;i++) {
-//           bigMatrix.push([lat, long + i]);
-//         }
-//         long = long + i;
-//         break;
-//     }
-//   })
-// });
-
-// var doubles = bigMatrix.reduce(function (out, item) {
-//   return out.concat(out.filter(function (comp) {
-//     console.log(item[0] == comp[0], item[1] == comp[1])
-//     return (item[0] == comp[0] && item[1] == comp[1]);
-//   }).length ? [] : [item])
-// }, []);
-
-// console.log(doubles);
-// bigMatrix.forEach(el =>{
-//   console.log(el);
-// })
-
 for(one in bigMatrix1) {
   // console.log(one);
   for(two in bigMatrix2) {
@@ -106,26 +57,21 @@ for(one in bigMatrix1) {
       break;
     }
   }
-
-  // var toto = bigMatrix2.map((item) => {
-  //   console.log(t);
-  //   console.log(item.toString(), bigMatrix1[one].toString())
-  //   if ((item.toString() === bigMatrix1[one].toString())) {
-  //     console.log(item);
-  //     break;
-  //   }
-  //   return (item.toString() === bigMatrix1[one].toString());
-  // })
-  // t++;
-  // console.log(toto);
 }
 
-console.log(bigMatrix)
+// it was [ -325, 384 ]
+// console.log(bigMatrix)
 
-// bigMatrix1.forEach(el =>{
-//   console.log(el);
-// })
-
-// bigMatrix2.forEach(el =>{
-//   console.log(el);
-// })
+// [ 0, 0 ]
+// [ -161, 1208 ]
+// [ -68, 1208 ]
+// [ -325, 648 ]
+// [ -325, 384 ]
+// [ -999, -113 ]
+// [ -1106, -148 ]
+// [ -2622, -1928 ]
+// [ -2661, -1595 ]
+// [ -2808, -1493 ]
+// [ -2193, -426 ]
+// [ -2193, -614 ]
+// [ -2164, -883 ]
